e6e6c3710182e79c648e5ef1f43bd878960e583d
Eric Yung <32915329+Nanomerce@users.noreply.github.com>
2020-01-14 17:11:35 -0500

    initial stuff

---
+++ src/main/include/subsystems/DriveTrain.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+
+namespace ohs2020 {
+    
+class DriveTrain : frc2::SubsystemBase {
+
+public:
+    DriveTrain();
+
+private:
+
+
+};
+
+}
