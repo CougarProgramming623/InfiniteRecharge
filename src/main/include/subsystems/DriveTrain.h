ace5f7a748aab578377bfe5da84ae8986b8f0ae4
Carson Harter <carsonharter@gmail.com>
2020-01-14 17:10:48 -0500

    Started to write the drive base

---
+++ src/main/include/subsystems/DriveTrain.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+
+
+namespace ohs2020 {
+
+class DriveTrain : frc2::SubsystemBase {
+
+public:
+    DriveTrain();
+    
+private:
+    
+
+}
+
+}
