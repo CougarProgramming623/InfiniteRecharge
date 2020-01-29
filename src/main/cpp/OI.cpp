b53abc4e1d47da2c0a81eac478bd33790a5149be
Carson Harter <carsonharter@gmail.com>
2020-01-28 19:58:07 -0500

    Working on FOD toggle button
    Not finished

--- src/main/cpp/OI.cpp
+++ src/main/cpp/OI.cpp
@@ -6,8 +6,7 @@ namespace ohs2020{
 OI::OI() : m_FodToggle(&m_DriverJoystick, 1) {
 	m_VelocityMode = true;
 
-	m_FodToggle.WhenPressed(new ToggleBoolean(&m_Fod), false);
-	
+	m_FodToggle.WhenPressed(new ToggleBoolean(&m_Fod, [](bool newValue){DebugOutF("Current Driver Orientation State: " + newValue ? "true" : "false");}), false);
 }
 
 void OI::Init(){
--- src/main/cpp/commands/ToggleBoolean.cpp
+++ src/main/cpp/commands/ToggleBoolean.cpp
@@ -1,17 +1,43 @@
 #include "commands/ToggleBoolean.h"
+#include "Util.h"
 
 namespace ohs2020{
 
 ToggleBoolean::ToggleBoolean(bool* toggle, std::function<void(bool newValue)> onFlip) : ptr(toggle), onFlip(onFlip) {
-
-
+	
 }
 
 void ToggleBoolean::Initialize() {
+	
 	*ptr = !ptr;
+	
+	DebugOutF("Ran Initialize");
 
 	onFlip(*ptr);
 
+	DebugOutF(std::to_string(*ptr));
+
+
 }
+
+bool ToggleBoolean::IsFinished(){ 
+	return true; 
+}
+
+
+void ToggleBoolean::End(bool interrupted) {}
+
+void ToggleBoolean::Execute(){
+
+}
+
+wpi::SmallSet<frc2::Subsystem*, 4> ToggleBoolean::GetRequirements() const{ 
+	wpi::SmallSet<frc2::Subsystem*, 4> set;
+	return set;
+}
+std::unique_ptr<frc2::Command> ToggleBoolean::TransferOwnership() &&{
+	return nullptr;
+}
+
 
 }//namespace
