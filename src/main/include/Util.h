415c32e3726102ed1059c8c04f9b60dd48bb145b
Carson Harter <carsonharter@gmail.com>
2020-02-12 17:53:07 -0500

    Added Wait() to Util.h

--- src/main/include/Util.h
+++ src/main/include/Util.h
@@ -2,7 +2,19 @@
 
 #include <frc2/command/PrintCommand.h>
 #include <frc2/command/CommandScheduler.h>
+#include <frc2/Timer.h>
 
 inline void DebugOutF(const wpi::Twine& message){
 	frc2::CommandScheduler::GetInstance().Schedule(new frc2::PrintCommand(message));
+}
+
+inline void Wait(double milliseconds) {
+
+	frc2::Timer timer;
+
+	timer.Reset();
+	timer.Start();
+
+	while(static_cast <double> (timer.Get()) * 1000 < milliseconds);
+
 }
