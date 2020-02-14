f6082089f48a25cd76020ff1e02edc6bab615f36
Carson Harter <carsonharter@gmail.com>
2020-02-14 17:44:31 -0500

    Updated climb

--- src/main/include/subsystems/Climb.h
+++ src/main/include/subsystems/Climb.h
@@ -6,6 +6,8 @@
 #include <frc2/command/Command.h>
 #include <frc2/command/FunctionalCommand.h>
 
+#include <frc2/Timer.h>
+
 #include "Util.h"
 
 namespace ohs2020{
@@ -19,14 +21,9 @@ public:
 	bool CanClimb();
 	void VerticalClimb();
 	void SideClimb();
-
-	bool isEndgame = false;
+	bool IsDeployed();
 
 private:
-
-	bool isClimbing = false;
-
-	int verticalDirection = 1;
 
 	WPI_TalonSRX climbMotorLeft;
 	WPI_TalonSRX climbMotorRight;
@@ -38,5 +35,9 @@ private:
 	frc2::Button climbRight;
 
 	frc2::Button endgameOverride;
+	frc2::Button deployer;
+	bool isDeployed = false;
+
+	frc2::Timer timer;
 };
 }//namespace
