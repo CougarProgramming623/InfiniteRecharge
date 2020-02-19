69700230a5c7abacaa2d4252e539a22736951bd2
Troy Neubauer <troyneubauer@gmail.com>
2020-02-19 16:50:43 -0500

    Switched #defines to const int

--- src/main/include/ButtonIDs.h
+++ src/main/include/ButtonIDs.h
@@ -1,26 +1,24 @@
 #pragma once
 
-#define JoystickID 				Robot::Get().GetOI().GetDriverJoystick().GetRawButton
-#define ButtonID 				Robot::Get().GetOI().GetButtonBoard().GetRawButton
 
-//climb	
-#define CLIMB_UP_ID				(19)	//Green Climb Button
-#define CLIMB_DOWN_ID			(20)	//Red Climb Button
-#define CLIMB_LEFT_ID			(21)
-#define CLIMB_RIGHT_ID			(22)
-#define DEPLOYER_ID				(17) //Drive Override Switch
-#define ENDGAME_OVERRIDE_ID		(18) //Endgame Override Switch
+//climb
+const int CLIMB_UP_ID			= 19;
+const int CLIMB_DOWN_ID			= 20;
+const int CLIMB_LEFT_ID			= 21;
+const int CLIMB_RIGHT_ID		= 22;
+const int DEPLOYER_ID			= 17;
+const int ENDGAME_OVERRIDE_ID	= 18;
 
 //shooter
-#define LAUNCHER_ID				(6) //Arm Override Button
-#define FLYWHEEL_TOGGLE_ID		(1) //Vacuum Switch
+const int LAUNCHER_ID			= 6;
+const int FLYWHEEL_TOGGLE_ID	= 1;
 
 //intake
-#define INTAKE_DOWN_ID			(10) //Bot Heading Dial
-#define INTAKE_UP_ID			(9)//Bot Heading Dial
-#define INTAKE_MANUAL_ID		(11)//Bot Heading Dial
-#define INTAKE_TOGGLE_ID		(11) //Fork Override Button
+const int INTAKE_DOWN_ID		= 10;
+const int INTAKE_UP_ID			= 9;
+const int INTAKE_MANUAL_ID		= 11;
+const int INTAKE_TOGGLE_ID		= 11;
 
 //OI
-#define FOD_TOGGLE_ID			(1) //Joystick Button
-#define TURN_ID					(14)//Hatch/Cargo Switch
+const int FOD_TOGGLE_ID			= 1;
+const int TURN_ID				= 1;
