218f19a838c3f63e8ce7e881701968a0a6b53892
Matthew Nicoud <nicoudm5@gmail.com>
2020-02-25 20:17:04 -0500

    pid tuning

--- src/main/cpp/commands/TurnToPosPID.cpp
+++ src/main/cpp/commands/TurnToPosPID.cpp
@@ -29,17 +29,17 @@ TurnToPosPID::TurnToPosPID(double angle) : frc2::PIDCommand(CreateTurnController
 
 void TurnToPosPID::Initialize() {
 
-	Robot::Get().GetDriveTrain().GetLBack()->ConfigPeakOutputForward(0.3, 0);
-	Robot::Get().GetDriveTrain().GetLBack()->ConfigPeakOutputReverse(-0.3, 0);
+	Robot::Get().GetDriveTrain().GetLBack()->ConfigPeakOutputForward(0.1, 0);
+	Robot::Get().GetDriveTrain().GetLBack()->ConfigPeakOutputReverse(-0.1, 0);
 
-	Robot::Get().GetDriveTrain().GetLFront()->ConfigPeakOutputForward(0.3, 0);
-	Robot::Get().GetDriveTrain().GetLFront()->ConfigPeakOutputReverse(-0.3, 0);
+	Robot::Get().GetDriveTrain().GetLFront()->ConfigPeakOutputForward(0.1, 0);
+	Robot::Get().GetDriveTrain().GetLFront()->ConfigPeakOutputReverse(-0.1, 0);
 
-	Robot::Get().GetDriveTrain().GetRBack()->ConfigPeakOutputForward(0.3, 0);
-	Robot::Get().GetDriveTrain().GetRBack()->ConfigPeakOutputReverse(-0.3, 0);
+	Robot::Get().GetDriveTrain().GetRBack()->ConfigPeakOutputForward(0.1, 0);
+	Robot::Get().GetDriveTrain().GetRBack()->ConfigPeakOutputReverse(-0.1, 0);
 
-	Robot::Get().GetDriveTrain().GetRFront()->ConfigPeakOutputForward(0.3, 0);
-	Robot::Get().GetDriveTrain().GetRFront()->ConfigPeakOutputReverse(-0.3, 0);
+	Robot::Get().GetDriveTrain().GetRFront()->ConfigPeakOutputForward(0.1, 0);
+	Robot::Get().GetDriveTrain().GetRFront()->ConfigPeakOutputReverse(-0.1, 0);
 
 
 	if (m_IsVision){
@@ -52,19 +52,19 @@ void TurnToPosPID::Initialize() {
 }
 
 void TurnToPosPID::Execute() {
+	if(m_IsVision)
+		m_Angle = GetVisionAngle();
 	frc2::PIDCommand::Execute();
-	m_OffsetValues[m_Index % 10] = m_Angle + m_Offset - Robot::Get().GetNavX()->GetYaw();
+	m_OffsetValues[m_Index % 10] = m_Angle + m_Offset - (double)Robot::Get().GetNavX()->GetYaw();
 	m_Index = m_Index + 1;
 }
 
 bool TurnToPosPID::IsFinished() {
-	DebugOutF("setpoint:" + std::to_string(m_Angle));
-	DebugOutF("point:" + std::to_string((double)Robot::Get().GetNavX()->GetYaw()));
-	DebugOutF(std::to_string(abs(m_Angle - (double)Robot::Get().GetNavX()->GetYaw())));
+	DebugOutF(std::to_string(abs(m_Angle + m_Offset - (double)Robot::Get().GetNavX()->GetYaw())));
 	if(m_Index < 12)
 		return false;
 	for(int i = 0; i < 10; i++) {
-		if(abs(m_OffsetValues[i]) > 2)
+		if(abs(m_OffsetValues[i]) > 0.5)
 			return false;
 	}
 	return true; //Robot::Get().GetOI().GetButtonBoard().GetRawButtonReleased(1);
@@ -97,10 +97,10 @@ frc2::PIDController TurnToPosPID::CreateTurnController() {
 	roughly accurate for greater than 10 : 0.085, 0.0, 0.011
 
 	Saber PID Values:
-	0.325, 0.0, 0.0125
+	0.8, 0.0, 0.04
 	*/
 
-    m_TurnController = new frc2::PIDController( 0.85, 0.0, 0.04, units::second_t(20_ms) );
+    m_TurnController = new frc2::PIDController( 0.85, 0.0, 0.0, units::second_t(20_ms) );
 
 	m_TurnController->SetTolerance( 2, std::numeric_limits< double >::infinity() );
 	m_TurnController->EnableContinuousInput(-180.0,180.0);
